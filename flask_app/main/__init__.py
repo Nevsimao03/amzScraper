d9f6e6e79396a1f74e3f1e5f7ce998ede54f2a29
Nevsimao <coday.ink@gmail.com>
2021-04-20 00:16:06 -0400

    got some data to display in to the client, a lot to work with still

--- flask_app/main/__init__.py
+++ flask_app/main/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-# init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
 
 def create_app():
@@ -11,13 +10,15 @@ def create_app():
     app.config['SECRET_KEY'] = 'secret-key-goes-here'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
+    app.config['ENV'] = 'development'
+    app.config['DEBUG'] = True
+    app.config['TESTING'] = True
+
     db.init_app(app)
 
-    # blueprint for auth routes in our app
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
-    # blueprint for non-auth parts of app
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
@@ -31,7 +32,6 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(user_id):
-        # since the user_id is just the primary key of our user table, use it in the query for the user
         return User.query.get(int(user_id))
 
     return app
--- flask_app/main/main.py
+++ flask_app/main/main.py
@@ -1,8 +1,22 @@
 from flask import Blueprint, render_template
 from . import db
 from flask_login import login_required, current_user
+import os
 
 main = Blueprint('main', __name__)
+
+filepath = os.path.join(os.path.dirname(__file__),'results.csv')
+open_read = open(filepath,'r')
+page =''
+
+while True:
+    read_data = open_read.readline()
+    page += '<p>%s</p>' % read_data
+    if open_read.readline() == '':
+        break
+@main.route("/data")
+def data():
+    return page
 
 @main.route('/')
 def index():
