b09b7c781e979b0e9f75f34382bb9903aa0b45cb
Nevsimao <coday.ink@gmail.com>
2021-04-21 03:09:25 -0400

    null

--- flask_app/main/main.py
+++ flask_app/main/main.py
@@ -2,21 +2,33 @@ from flask import Blueprint, render_template
 from . import db
 from flask_login import login_required, current_user
 import os
+import pandas as pd
 
 main = Blueprint('main', __name__)
 
-filepath = os.path.join(os.path.dirname(__file__),'results.csv')
-open_read = open(filepath,'r')
-page =''
+# filepath = os.path.join(os.path.dirname(__file__),'results.csv')
+# open_read = open(filepath,'r')
+# page =''
 
-while True:
-    read_data = open_read.readline()
-    page += '<p>%s</p>' % read_data
-    if open_read.readline() == '':
-        break
+# while True:
+#     read_data = open_read.readline()
+#     page += '<p>%s</p>' % read_data
+#     if open_read.readline() == '':
+#         break
+# @main.route("/data")
+# def data():
+#     return page
+
 @main.route("/data")
 def data():
-    return page
+    def show_tables():
+        data = pd.read_csv('results.csv')
+        data.set_index(['Name'], inplace=True)
+        data.index.name=None
+        description = data.loc[data.Description]
+        price = data.loc[data.Price]
+        return render_template('dataset.html',tables=[description.to_html(classes='description'), price.to_html(classes='price')],
+        titles = ['na', 'Description', 'Price'])
 
 @main.route('/')
 def index():
